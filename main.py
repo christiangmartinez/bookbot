400b1077f0b3a1e1b69acdb8ba2011251f63dec6
Christian Martinez <cgmartinez11142@gmail.com>
2025-04-18 12:13:34 -0700

    add sys.arvg

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import sys
+
 from stats import get_char_count, get_num_words, get_sorted_char_count
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = (sys.argv[1])
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_char_count(text)
@@ -24,3 +30,4 @@ def get_book_text(path):
         return f.read()
 
 main()
+
