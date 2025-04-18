73dbb068c4e6b61141f69e9e0236d2418248014e
Christian Martinez <cgmartinez11142@gmail.com>
2025-04-18 10:02:21 -0700

    refactor

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from stats import get_char_count, get_num_words
+from stats import get_char_count, get_num_words, get_sorted_char_count
 
 
 def main():
-    get_num_words('./books/frankenstein.txt')
-    get_char_count('./books/frankenstein.txt')
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_char_count(text)
+    #get_sorted_char_count(chars_dict)
+    print(f"{num_words} words found in the document")
+    print(chars_dict)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
--- stats.py
+++ stats.py
@@ -1,24 +1,29 @@
-def get_num_words(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-        word_list = file_contents.split()
-        num_words = len(word_list)
-        print(f"{num_words} words found in the document")
+def get_num_words(text):
+    word_list = text.split()
+    return len(word_list)
 
-def get_char_count(filepath):
-    with open(filepath) as f:
-        file_contents = f.read().lower()
-        letters = list(file_contents)
+def get_char_count(text):
+    chars = {}
 
-        chars = {}
+    for c in text:
+        letter = c.lower()
+        if letter in chars:
+            chars[letter] += 1
+        else:
+            chars[letter] = 1
 
-        for letter in letters:
-            if letter in chars:
-                chars[letter] += 1
-            else:
-                chars[letter] = 1
+    return chars
 
-        for char in chars:
-            count = chars[char]
-            print(f"'{char}': {count}")
+
+def get_sorted_char_count(chars):
+    char_list = []
+
+    for char in chars:
+        new_dict = { char: chars[char] }
+        char_list.append(new_dict)
+
+    char_list.sort()
+    for letter in char_list:
+        if letter.isalpha():
+            print(letter)
 
